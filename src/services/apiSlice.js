280d8ecd3b8be757267a2aef0a1cc2f8affb1d75
Ashik047 <ashikbiju2000@gmail.com>
2025-09-09 00:38:03 +0530

    FrontEnd First Commit

--- src/services/apiSlice.js
+++ src/services/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 export const apiSlice = createApi({
     reducerPath: 'api',
-    baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:3000' }),
+    baseQuery: fetchBaseQuery({ baseUrl: 'https://fintrack-backend-e20c.onrender.com' }),
     tagTypes: ['Money'],
     endpoints: (builder) => ({
         getActivities: builder.query({
